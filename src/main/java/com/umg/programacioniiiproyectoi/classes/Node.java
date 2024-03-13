50695f8b31d7694eff6de12a1322e4261d65eea0
estiven-lg <estivenlaferre@gmail.com>
2024-03-12 23:26:49 -0600

    corrections

--- src/main/java/com/umg/programacioniiiproyectoi/classes/Node.java
+++ src/main/java/com/umg/programacioniiiproyectoi/classes/Node.java
@@ -10,63 +10,41 @@ public class Node {
 	private String value;
 
 	public Node(String value) {
-		this.value = value;
+	
 	}
 
 	public Node getLeft() {
-		return left;
+		return null;
 	}
 
 	public void setLeft(Node left) {
-		this.left = left;
+	
 	}
 
 	public Node getRight() {
-		return right;
+		return null;
 	}
 
 	public void setRight(Node right) {
-		this.right = right;
+
 	}
 
 	public String getLabel() {
-		return label;
+		return null;
 	}
 
 	public void setLabel(String label) {
-		this.label = label;
+	
 	}
 
 	public String getValue() {
-		return value;
+		return null;
 	}
 
 	public void setValue(String value) {
-		this.value = value;
+	
 	}
 
-	private String toString(Node node, int level, String arrow) {
-		String text = "";
-		if (node.getLeft() != null) {
-			text += toString(node.getLeft(), (level + 1), "-->");
-		}
-
-		for (int i = 0; i < level * 2; i++) {
-			text += " ";
-		}
-		text += arrow + node.value + "\n";
-
-		if (node.getRight() != null) {
-			text += toString(node.getRight(), (level + 1), "-<>");
-		}
-
-		return text;
-	}
-
-	@Override
-	public String toString() {
-		return this.toString(this, 0, "");
-	}
 
 	static public Node createTree(String operation) {
 		Stack<Node> tmpStack = new Stack<Node>();
